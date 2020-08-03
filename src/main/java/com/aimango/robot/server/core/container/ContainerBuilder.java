b8be0fc5a0639ba2101fc03b514a70836179391a
DavidLiu <1049698300@qq.com>
2020-08-03 10:17:37 +0800

    移除 Reflections工具，替换为自己编写的类扫描工具

--- src/main/java/com/aimango/robot/server/core/container/ContainerBuilder.java
+++ src/main/java/com/aimango/robot/server/core/container/ContainerBuilder.java
@@ -1,20 +1,33 @@
 package com.aimango.robot.server.core.container;
 
+import com.aimango.robot.server.core.annotation.Component;
 import com.aimango.robot.server.core.annotation.Controller;
 import com.aimango.robot.server.core.scanner.ClassScanner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.util.HashSet;
 import java.util.Set;
 
 public class ContainerBuilder implements Builder<Container>{
+    private static final String PACKAGE="com.aimango.robot.server";
     private static final Logger logger= LoggerFactory.getLogger(ContainerBuilder.class);
 
     @Override
-    public Container build() throws InstantiationException, IllegalAccessException {
+    public Container build() throws InstantiationException, IllegalAccessException, IOException, ClassNotFoundException {
         logger.info("构建容器");
         //扫描所有@Component的类
-        Set<Class> classes = ClassScanner.scanComponents();
+        Set<Class> classSet = ClassScanner.scan(PACKAGE);
+        Set<Class> classes=new HashSet<>();
+        for (Class clazz:classSet){
+            boolean annotateWith = ClassScanner.isAnnotateWith(clazz, Component.class);
+            if (annotateWith){
+                classes.add(clazz);
+            }
+        }
+
+
         FullClassContainer fullClassContainer=new FullClassContainer(classes);
         return fullClassContainer;
     }
--- src/main/java/com/aimango/robot/server/core/scanner/ClassScanner.java
+++ src/main/java/com/aimango/robot/server/core/scanner/ClassScanner.java
@@ -1,34 +1,101 @@
 package com.aimango.robot.server.core.scanner;
 
-import com.aimango.robot.server.core.annotation.Component;
-import org.reflections.Reflections;
-import org.reflections.util.ClasspathHelper;
-import org.reflections.util.ConfigurationBuilder;
-
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Enumeration;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 public class ClassScanner {
+    /**
+     * 获取类路径下某个包下的所有类
+     * @param packageName
+     * @return
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    public static Set<Class> scan(String packageName) throws IOException, ClassNotFoundException {
+        Set<Class> classes=new HashSet<>();
+        String packagePath = packageName.replace(".", "/");
+        Enumeration<URL> resources = Thread.currentThread().getContextClassLoader().getResources("");
+        while (resources.hasMoreElements()){
+            URL url = resources.nextElement();
+            URLClassLoader urlClassLoader=new URLClassLoader(new URL[]{url}, Thread.currentThread().getContextClassLoader());
+            System.out.println(url.toString());
 
-    public static Set<Class> scanComponents() {
-        Set<Class> classSet=new HashSet<>();
-        String classpath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
-        System.setProperty("java.class.path",classpath);
-        Reflections reflections=new Reflections(new ConfigurationBuilder().setUrls(ClasspathHelper.forJavaClassPath()));
-
-        Set<Class<?>> typesAnnotatedWith = reflections.getTypesAnnotatedWith(Component.class);
-
-        Iterator<Class<?>> iterator = typesAnnotatedWith.iterator();
-        while (iterator.hasNext()){
-            Class next = iterator.next();
-            if (!(next.isAnnotation()||next.isEnum()||next.isInterface())){
-                classSet.add(next);
+            String protocol = url.getProtocol();
+            System.out.println("协议："+protocol);
+            if ("jar".equals(protocol)){
+                JarURLConnection jarURLConnection=(JarURLConnection) url.openConnection();
+                JarFile jarFile = jarURLConnection.getJarFile();
+                Enumeration<JarEntry> entries = jarFile.entries();
+                while (entries.hasMoreElements()){
+                    JarEntry jarEntry = entries.nextElement();
+                    String name = jarEntry.getName();
+                    int index = name.indexOf(packagePath);
+                    if (index!=-1&&name.endsWith(".class")){
+                        String replace = name.substring(index, name.length() - 6).replace("/", ".");
+                        Class clazz = urlClassLoader.loadClass(replace);
+                        classes.add(clazz);
+                    }
+                }
+            }else if("file".endsWith(protocol)){
+                String path = url.getPath();
+                String targetPath = path + "/" + packagePath;
+                addClasses(targetPath,classes,packageName);
             }
         }
-        if (classSet.size()<=0){
-            return null;
+        return classes;
+    }
+
+    private static void addClasses(String path, Set<Class> classes, String packageName) throws ClassNotFoundException {
+        File[] files = new File(path).listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File file) {
+                return (file.isFile() && file.getName().endsWith(".class")) || file.isDirectory();
+            }
+        });
+
+        for (File file:files){
+            String fileName = file.getName();
+            if (file.isFile()){
+                String className = fileName.substring(0, fileName.lastIndexOf("."));
+                String fullClassName = packageName + "." + className;
+                Class clazz = Thread.currentThread().getContextClassLoader().loadClass(fullClassName);
+                classes.add(clazz);
+            }else {
+                String subPackagePath = path + "/" + fileName;
+                String subPackageName = packageName + "." + fileName;
+                addClasses(subPackagePath,classes,subPackageName);
+            }
         }
-        return classSet;
+    }
+
+    public static boolean isAnnotateWith(Class clazz, Class targetAnnotation){
+        Annotation[] annotations = clazz.getAnnotations();
+        for (Annotation annotation:annotations){
+            Class<? extends Annotation> aClass = annotation.annotationType();
+            if (aClass.equals(targetAnnotation)){
+                return true;
+            }
+            if (aClass.isAnnotationPresent(targetAnnotation)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Set<Class> scan = ClassScanner.scan("com.aimango.server.component");
+        for (Class clazz:scan){
+            System.out.println(clazz.getName());
+        }
     }
 }
